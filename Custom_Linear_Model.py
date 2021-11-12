add6df16207e23463fb96af2f80365a7d3475c88
cc-21 <55864124+cc-21@users.noreply.github.com>
2021-11-12 05:35:53 -0500

    Add custom linear model

---
+++ Custom_Linear_Model.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+
+
+def ordinary_least_square_loss(predicted, actual, sample_weights=None):
+    sum_squared_error = np.sum((predicted - actual)**2)
+    mean_error = sum_squared_error / float(len(actual))
+    return mean_error
+
+
+def mean_absolute_percentage_loss(y_pred, y_true, sample_weights=None):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    assert len(y_true) == len(y_pred)
+
+    if np.any(y_true == 0):
+        print("Found zeroes in y_true. MAPE undefined. Removing from set...")
+        idx = np.where(y_true == 0)
+        y_true = np.delete(y_true, idx)
+        y_pred = np.delete(y_pred, idx)
+        if type(sample_weights) != type(None):
+            sample_weights = np.array(sample_weights)
+            sample_weights = np.delete(sample_weights, idx)
+
+    if type(sample_weights) == type(None):
+        return (np.mean(np.abs((y_true - y_pred) / y_true)) * 100)
+    else:
+        sample_weights = np.array(sample_weights)
+        assert len(sample_weights) == len(y_true)
+        return (100 / sum(sample_weights) * np.dot(
+            sample_weights, (np.abs((y_true - y_pred) / y_true))
+        ))
+
+
+class CustomLinearModel:
+    """
+    Linear model: Y = XB, fit by minimizing the provided loss_function with L2 regularization
+    """
+
+    def __init__(self, loss_function=ordinary_least_square_loss,
+                 X=None, Y=None, sample_weights=None, coeff_init=None, regularization=0.00012, rho=1):
+        self.regularization = regularization
+        self.coeff = None
+        self.loss_function = loss_function
+        self.sample_weights = sample_weights
+        self.coeff_init = coeff_init
+        self.rho = rho
+
+        self.X = X
+        self.Y = Y
+
+    def predict(self, X):
+        prediction = np.matmul(X, self.coeff)
+        return (prediction)
+
+    def model_error(self):
+        error = self.loss_function(
+            self.predict(self.X), self.Y, sample_weights=self.sample_weights
+        )
+        return (error)
+
+    def elastic_net_penalty(self, coeff):
+        self.coeff = coeff
+        return self.model_error() + (1-self.rho) * self.l1_regularization() + self.rho * self.l2_regularization()
+
+    def l2_regularization(self):
+        return sum(self.regularization * np.array(self.coeff) ** 2)
+
+    def l1_regularization(self):
+        return sum(self.regularization * abs(self.coeff))
+
+    def fit(self, maxiter=250):
+        # Initialize coeff estimates (you may need to normalize your data and choose smarter initialization values
+        # depending on the shape of your loss function)
+        if type(self.coeff_init) == type(None):
+            # set coeff_init = 1 for every feature
+            self.coeff_init = np.array([1] * self.X.shape[1])
+        else:
+            # Use provided initial values
+            pass
+
+        if self.coeff is not None and all(self.coeff_init == self.coeff):
+            print("Model already fit once; continuing fit with more iterations.")
+
+        res = minimize(self.elastic_net_penalty, self.coeff_init, method='BFGS', options={'maxiter': maxiter})
+        self.coeff = res.x
+        self.coeff_init = self.coeff
+
+
+# Calculate root mean squared error
+def rsqr_metric(predicted, actual):
+    sum_error = np.sum((predicted - actual)**2)
+    mean_error = sum_error / np.sum(actual ** 2)
+    return 1-mean_error
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    X_df = pd.read_csv("full_predictor_set_bfill.csv", index_col=0)
+    y_df = pd.read_csv("returns.csv", index_col=0)
+
+    X = X_df.to_numpy()
+    y = y_df.to_numpy()
+
+    print(X.shape)
+    print(y.shape)
+
+    X_train = X[0:1744000,2:]
+    y_train = y[0:1744000,2:].flatten()
+
+    print(X_train.shape)
+    print(y_train.shape)
+
+    l2_model = CustomLinearModel(
+        loss_function=ordinary_least_square_loss,
+        X=X_train, Y=y_train, regularization=0.00012
+    )
+    l2_model.fit()
+    print(l2_model.coeff)
+
+    print(rsqr_metric(np.dot(X_train, l2_model.coeff), y_train))
